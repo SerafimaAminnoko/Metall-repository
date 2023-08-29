39a6b53817a1ecd6c63c60ff204c4d6da0bdfa5e
SerafimaAminnoko <132136596+SerafimaAminnoko@users.noreply.github.com>
2023-08-29 11:32:38 +0300

    add new Group API method=GET

--- song_project/songs/views.py
+++ song_project/songs/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from rest_framework.response import Response
+
+
+from .serializers import *
+
+
+class GroupAPIView(generics.ListAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
